e00e17cef8012100a54d2550addab881692baa34
zakaria <zakariaselassi@gmail.com>
2021-12-28 19:30:20 +0100

    update : done

--- angular-steps/src/app/components/header/header.component.ts
+++ angular-steps/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Subscription} from 'rxjs'
 import {UiService} from '../../services/ui.service'
+import {Router} from  '@angular/router'
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -12,8 +13,9 @@ export class HeaderComponent implements OnInit {
   subscription!: Subscription;
   title: string = 'aangular-steps'
 
-  constructor(private uiService:UiService) { 
+  constructor(private uiService:UiService,private router:Router) { 
     this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+    
   }
 
   ngOnInit(): void {
@@ -21,5 +23,8 @@ export class HeaderComponent implements OnInit {
   toggleAddTask(): void{
     this.uiService.toggleAddTask()
   }
-
+  hasRoute(route:string):boolean{
+    console.log("route", route, this.router.url)
+    return this.router.url === route;
+  }
 }
